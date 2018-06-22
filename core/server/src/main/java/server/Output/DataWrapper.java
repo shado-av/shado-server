150858c7a82089de5e3207da573424059cce2ecd
Naixin <ny32@duke.edu>
2018-06-22 11:58:20 -0400

    create the package

--- core/server/src/main/java/server/Output/DataWrapper.java
+++ core/server/src/main/java/server/Output/DataWrapper.java
@@ -40,8 +40,8 @@ public class DataWrapper {
 
     public DataWrapper(Simulation o, loadparam param) {
         stdout = System.out;
-        outPutDirectory = "/Users/zhanglian1/Desktop/out/";
-//        outPutDirectory = "/home/rapiduser/shado-server/core/server/out/";
+//        outPutDirectory = "/Users/zhanglian1/Desktop/out/";
+        outPutDirectory = "/home/rapiduser/shado-server/core/server/out/";
         vars = param;
         sim = o;
         numSpecialTasks = o.getNumSpecialTasks();
