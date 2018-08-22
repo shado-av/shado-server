f5cceb9e7f554c28f83dd19b62ffab2d938c5384
hanwiz <hanwiz@gmail.com>
2018-08-22 12:14:14 -0400

    Remove autoconnection to db

--- core/server/src/main/java/server/Application.java
+++ core/server/src/main/java/server/Application.java
@@ -2,8 +2,10 @@ package server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.*;
+import org.springframework.boot.autoconfigure.data.mongo.*;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
